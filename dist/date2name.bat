e44d6f5d669becdd607180d64c2cce944ed9ea65
Reiner Rottmann <reiner@rottmann.it>
2021-06-18 22:05:49 +0200

    added a windows dist for my personal use.

---
+++ dist/date2name.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+REM change drive, e.g., D:
+%~d1
+REM change directory, e.g., D:\data processing\subdir\
+cd %~dp1
+
+
+"C:\Users\rottmrei\OneDrive\Tools\210618-date2name\date2name.exe" %*
+
+set /p DUMMY=Hit ENTER to continue...
